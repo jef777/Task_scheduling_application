c96bea448baea571c6e46a3b94b783d79913cba5
jeff <jeffmuigai11@gmail.com>
2022-03-27 17:48:50 +0300

    deployment configs

--- src/server/app/config/db.config.js
+++ src/server/app/config/db.config.js
@@ -1,26 +1,10 @@
 // require("dotenv").config();
 
-// module.exports = {
-//   HOST: "localhost",
-//   USER: "jeff",
-//   PASSWORD: "jeffc123",
-//   DB: "jeff",
-//   dialect: "postgres",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
-
 module.exports = {
-  dbname: "d23ank0fg7nedj",
-  host: "ec2-52-73-155-171.compute-1.amazonaws.com",
-  port: "5432",
-  user: "rwdfseugzpyyml",
-  password: "d7eac35388cf636ea2ea8fbf4b4aa88c6d7ed4185f97f49fc5257829b885bd21",
-  sslmode: "require",
+  HOST: "localhost",
+  USER: "jeff",
+  PASSWORD: "jeffc123",
+  DB: "jeff",
   dialect: "postgres",
   pool: {
     max: 5,
@@ -29,3 +13,19 @@ module.exports = {
     idle: 10000
   }
 };
+
+// module.exports = {
+//   dbname: "d23ank0fg7nedj",
+//   host: "ec2-52-73-155-171.compute-1.amazonaws.com",
+//   port: "5432",
+//   user: "rwdfseugzpyyml",
+//   password: "d7eac35388cf636ea2ea8fbf4b4aa88c6d7ed4185f97f49fc5257829b885bd21",
+//   sslmode: "require",
+//   dialect: "postgres",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
